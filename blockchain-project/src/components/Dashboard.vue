383cbbbc1dc2ab44ee6a0e66e2e8c2e4cf2651af
zhyu1207 <zhy47@126.com>
2019-03-22 22:41:31 +0800

    Initial commit

---
+++ blockchain-project/src/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+  <h3>Dashboard</h3>
+</div>
+</template>
+<script>
+export default {
+  name: 'Dashboard',
+  data () {
+
+  }
+}
+</script>
--- blockchain-project/src/router/index.js
+++ blockchain-project/src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import NewUser from '@/components/NewUser'
+import ViewUser from '@/components/ViewUser'
+import EditUser from '@/components/EditUser'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/new',
+      name: 'new-user',
+      component: NewUser
+    },
+    {
+      path: '/:user_id',
+      name: 'view-user',
+      component: ViewUser
+    },
+    {
+      path: '/edit/:user_id',
+      name: 'edit-user',
+      component: EditUser
     }
   ]
 })
