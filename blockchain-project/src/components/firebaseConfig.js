06a48d4921db50737965c998ecb4b3ca4282951b
huiyeetan <huiyeetan@hotmail.com>
2019-03-22 22:32:06 +0800

    Added firebase db

---
+++ blockchain-project/src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+
+  apiKey: "AIzaSyAvU-1ARnvEdAYMNg5CwcMN3_LLd-_Ch8g",
+  authDomain: "vuefs-dev-838e0.firebaseapp.com",
+  databaseURL: "https://vuefs-dev-838e0.firebaseio.com",
+  projectId: "vuefs-dev-838e0",
+  storageBucket: "vuefs-dev-838e0.appspot.com",
+  messagingSenderId: "239008336398"
+  
+}
