17399ea4757355613a3c8303c49fc213dc81b871
zhyu1207 <zhy47@126.com>
2019-03-23 10:24:20 +0800

    Add Navbar functionality. Add floating button

--- blockchain-project/src/components/Dashboard.vue
+++ blockchain-project/src/components/Dashboard.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
   <h3>Dashboard</h3>
+   <div class="fixed-action-btn">
+    <router-link to="/new" class="btn-floating btn-large red">
+      <i class="fa fa-plus"></i>
+    </router-link>
+  </div>
 </div>
 </template>
 <script>
---
+++ blockchain-project/src/components/NewUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>New User</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ViewUser',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
