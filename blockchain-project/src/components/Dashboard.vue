9db141f37973d80fb863a10fc044f0b5b2a1f02f
zhyu1207 <zhy47@126.com>
2019-03-23 14:15:22 +0800

    Finish part 4. Add edit & delete functionality to user

--- blockchain-project/src/components/Dashboard.vue
+++ blockchain-project/src/components/Dashboard.vue
@@ -1,25 +1,27 @@
 <template>
-  <div>
-  <ul class="collection with-header">
-    <li class="collection-header"><h4>Users</h4></li>
-    <li v-for="user in users" v-bind:key="user.id" class="collection-item">
-      <div class="chip">{{user.id}}</div>
-      <div class="chip">{{user.email}}</div>
-       {{user.name}}, {{user.contact_number}}
-        <router-link class="secondary-content" v-bind:to="{ name: 'view-user', params: { user_id: user.id }}"><i class="fa fa-eye"></i></router-link>
-    </li>
-  </ul>
-   <div class="fixed-action-btn">
+  <div id="dashboard">
+    <ul class="collection with-header">
+      <li class="collection-header"><h4>Users</h4></li>
+      <li v-for="user in users" v-bind:key="user.id" class="collection-item">
+        <div class="chip">{{user.id}}</div>
+        <div class="chip">{{user.email}}</div>
+        {{user.name}}, {{user.contact_number}}
+          <router-link class="secondary-content" v-bind:to="{ name: 'view-user', params: { user_id: user.id }}">
+            <i class="fa fa-eye"></i>
+          </router-link>
+      </li>
+    </ul>
+    <div class="fixed-action-btn">
     <router-link to="/new" class="btn-floating btn-large red">
       <i class="fa fa-plus"></i>
     </router-link>
+    </div>
   </div>
-</div>
 </template>
 <script>
 import db from './firebaseInit'
 export default {
-  name: 'Dashboard',
+  name: 'dashboard',
   data () {
     return {
       users: []
@@ -28,9 +30,9 @@ export default {
   created () {
     db.collection('users').orderBy('email', 'desc').get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        console.log(doc)
+        // console.log(doc)
         const data = {
-          'id': doc.id,
+          'id': doc.data().user_id,
           'name': doc.data().name,
           'email': doc.data().email,
           'contact_number': doc.data().contact_number
--- blockchain-project/src/components/ViewUser.vue
+++ blockchain-project/src/components/ViewUser.vue
@@ -1,14 +1,62 @@
 <template>
-  <div>
-    <h3>ViewUser</h3>
-  </div>
+<div class="view-user">
+    <ul class="collection with-header">
+      <li class="collection-header"><h4>{{name}}</h4></li>
+      <li class="collection-item">ID#: {{id}}</li>
+      <li class="collection-item">E-mail: {{email}}</li>
+      <li class="collection-item">Tel: {{contact_number}}</li>
+    </ul>
+    <router-link to="/" class="btn grey">Back</router-link>
+    <button @click="deleteUser" class="btn red">Delete</button>
+</div>
 </template>
 <script>
+import db from './firebaseInit'
 export default {
-  name: 'ViewUser',
+  name: 'view-user',
   data () {
     return {
-
+      'id': null,
+      'name': null,
+      'email': null,
+      'contact_number': null
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    db.collection('users').where('user_id', '==', to.params.user_id).get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        next(vm => {
+          vm.id = doc.data().user_id
+          vm.name = doc.data().name
+          vm.contact_number = doc.data().contact_number
+          vm.email = doc.data().email
+        })
+      })
+    })
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      db.collection('users').where('user_id', '==', this.$route.params.user_id).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.id = doc.data().user_id
+          this.name = doc.data().name
+          this.email = doc.data().email
+          this.contact_number = doc.data().contact_number
+        })
+      })
+    },
+    deleteUser () {
+      if (confirm('Are you sure?')) {
+        db.collection('users').where('user_id', '==', this.$route.params.user_id).get().then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            doc.ref.delete()
+            this.$router.push('/')
+          })
+        })
+      }
     }
   }
 }
