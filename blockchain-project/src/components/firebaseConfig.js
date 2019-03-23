50c6070b2df6619174cdf453b9ad88ebe594e158
zhyu1207 <zhy47@126.com>
2019-03-23 11:55:00 +0800

    Finish tutorial part 3. Add interactive user table to dashboard

--- blockchain-project/src/components/firebaseConfig.js
+++ blockchain-project/src/components/firebaseConfig.js
@@ -1,8 +1,8 @@
-export default { 
-    apiKey: "AIzaSyBa0fx4Rvdr3YdnvPKQriJ5c3XzaPUwmI4",
-    authDomain: "is4302.firebaseapp.com",
-    databaseURL: "https://is4302.firebaseio.com",
-    projectId: "is4302",
-    storageBucket: "is4302.appspot.com",
-    messagingSenderId: "332381728433"
+export default {
+  apiKey: 'AIzaSyBa0fx4Rvdr3YdnvPKQriJ5c3XzaPUwmI4',
+  authDomain: 'is4302.firebaseapp.com',
+  databaseURL: 'https://is4302.firebaseio.com',
+  projectId: 'is4302',
+  storageBucket: 'is4302.appspot.com',
+  messagingSenderId: '332381728433'
 }
