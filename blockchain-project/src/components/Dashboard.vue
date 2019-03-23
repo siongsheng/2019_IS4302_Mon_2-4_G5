f0eb0da718f0a5b8f80e58551f21f2882b930dca
zhyu1207 <zhy47@126.com>
2019-03-23 15:04:35 +0800

    Finish part 5. Added edit user and add new user functionality

--- blockchain-project/src/components/Dashboard.vue
+++ blockchain-project/src/components/Dashboard.vue
@@ -12,9 +12,9 @@
       </li>
     </ul>
     <div class="fixed-action-btn">
-    <router-link to="/new" class="btn-floating btn-large red">
-      <i class="fa fa-plus"></i>
-    </router-link>
+      <router-link to="/new" class="btn-floating btn-large red">
+        <i class="fa fa-plus"></i>
+      </router-link>
     </div>
   </div>
 </template>
--- blockchain-project/src/components/ViewUser.vue
+++ blockchain-project/src/components/ViewUser.vue
@@ -8,6 +8,11 @@
     </ul>
     <router-link to="/" class="btn grey">Back</router-link>
     <button @click="deleteUser" class="btn red">Delete</button>
+    <div class="fixed-action-btn">
+      <router-link v-bind:to="{ name: 'edit-user', params: { user_id: id }}" class="btn-floating btn-large red">
+        <i class="fa fa-pencil"></i>
+      </router-link>
+    </div>
 </div>
 </template>
 <script>
