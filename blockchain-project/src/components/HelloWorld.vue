ad2698de7e4a2d80985a2832b307fd9c4c521f70
royamsoft tech <anusree0614@gmail.com>
2019-03-27 00:55:23 +0800

    client can connect to server

--- blockchain-project/src/components/HelloWorld.vue
+++ blockchain-project/src/components/HelloWorld.vue
@@ -1,95 +1,47 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div align="center">
+      <div class="table">
+        <h3>Testing the server</h3>
+        <thead>
+          <tr>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Product Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{Product.Id}}</td>
+            <td>{{Product.name}}</td>
+            <td>{{Product.price}}</td>
+          </tr>
+        </tbody>
+
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      Product: {}
     }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/hlf/api/org.deliverlor.ecommerce.Product')
+    .then((response) => {
+      console.log(response.data[0]);
+      this.Product = response.data[0];
+    })
+    .catch(error => {
+      console.log(error);
+    })
   }
 }
 </script>
