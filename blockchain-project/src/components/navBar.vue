d35d8ae37b30a06e1a1542fdac02460ce6c118b5
huiyeetan <huiyeetan@hotmail.com>
2019-03-26 11:13:50 +0800

    merge master 2 with master branch

--- blockchain-project/src/components/navBar.vue
+++ blockchain-project/src/components/navBar.vue
@@ -1,13 +1,45 @@
 <template>
 <nav>
-  <div id="navBar">
-    <div class="nav-wrapper">
+    <div class="nav-wrapper green">
       <div class="container">
-        <router-link to="/"
-        class="brand-logo">Ecommerce Blockchain</router-link>
+        <router-link to="/" class="brand-logo">DeliverLor</router-link>
+        <img :src="image" style="width:3em; height:2em; margin-left:160px; margin-top:18px;">
+        <ul class="right">
+          <li v-if="isLoggedIn"><router-link to="/">Dashboard</router-link></li>
+          <!-- <li v-if="!isLoggedIn"><router-link to="/login">Login</router-link></li> -->
+          <li v-if="isLoggedIn">
+            <button v-on:click="logout" class="btn black">Logout</button>
+          </li>
+
+        </ul>
       </div>
     </div>
-  </div>
 </nav>
-
 </template>
+<script>
+  import firebase from 'firebase'
+  export default{
+    name: 'navbar',
+    data(){
+      return{
+        isloggedIn: false,
+        currentUser: false,
+        image: require('@/assets/Delivery.png')
+      }
+    },
+    created(){
+      if(firebase.auth().currentUser){
+        this.isLoggedIn = true;
+        this.currentUser = firebase.auth().currentUser.email;
+      }
+    },
+    methods:{
+      logout: function(){
+        firebase.auth().signOut().then(() => {
+          this.$router.go({path: this.$router.path});
+        });
+      }
+    }
+  }
+
+</script>
