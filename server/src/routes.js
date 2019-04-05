3ecaba51f229c5aa1c07dc85fdb3d8bdc5180e48
Julian Ng <ngvoonchoong@gmail.com>
2019-04-06 00:53:17 +0800

    update product, logsreq, offer
    update product with PUT /:user/product/:product_id
    update logsreq desiredPrice with PUT /:user/logisticsrequest/:logisticsRequest_id
    create and update offer with PUT /:user/offerTx/ (**NOTE**: there is no more POST. PUT is idempotent and will be used to create and update)
    rmb to redeploy the new bna from master branch!

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,24 +2,19 @@ const UsersController = require('./controllers/UsersController')
 const OrderController = require('./controllers/OrderController')
 
 module.exports = (router) => {
-	router.get('/:user/buyer', UsersController.buyers)
-	router.get('/:user/buyer/:buyer_id', UsersController.buyer)
-	router.param('buyer_id', function(req, res, next, buyer_id) {
-		req.body.buyer_id = buyer_id
-		next();
-	});
+	router.get('/:user/buyer', UsersController.buyer)
 	router.get('/:user/seller', UsersController.sellers)
 	router.get('/:user/seller/:seller_id', UsersController.seller)
 	router.param('seller_id', function(req, res, next, seller_id) {
 		req.body.seller_id = seller_id
-		next();
-	});
+		next()
+	})
 	router.get('/:user/logistics', UsersController.logisticss)
 	router.get('/:user/logistics/:logs_id', UsersController.logistics)
 	router.param('logs_id', function(req, res, next, logs_id) {
 		req.body.logs_id = logs_id
-		next();
-	});
+		next()
+	})
 
 	router.route('/:user/wallet')
 		.get(UsersController.getWallet)
@@ -28,11 +23,13 @@ module.exports = (router) => {
 	router.route('/:user/product')
 		.get(OrderController.products)
 		.post(OrderController.postProduct)
-	router.get('/:user/product/:product_id', OrderController.product)
+	router.route('/:user/product/:product_id')
+		.get(OrderController.product)
+		.put(OrderController.putProduct)
 	router.param('product_id', function(req, res, next, product_id) {
 		req.body.product_id = product_id
-		next();
-	});
+		next()
+	})
 
 	router.route('/:user/order')
 		.get(OrderController.orders)
@@ -40,17 +37,19 @@ module.exports = (router) => {
 	router.get('/:user/order/:order_id', OrderController.order)
 	router.param('order_id', function(req, res, next, order_id) {
 		req.body.order_id = order_id
-		next();
-	});
+		next()
+	})
 
 	router.get('/:user/logisticsrequest', OrderController.logisticsrequests)
-	router.get('/:user/logisticsrequest/:logisticsRequest_id', OrderController.logisticsrequest)
+	router.route('/:user/logisticsrequest/:logisticsRequest_id')
+		.get(OrderController.logisticsrequest)
+		.put(OrderController.putLogisticsrequest)
 	router.param('logisticsRequest_id', function(req, res, next, logisticsRequest_id) {
 		req.body.logisticsRequest_id = logisticsRequest_id
-		next();
-	});
+		next()
+	})
 
-	router.post('/:user/offerTx', OrderController.offerTx)
+	router.put('/:user/offerTx', OrderController.offerTx)
 	router.post('/:user/acceptOfferTx', OrderController.acceptOfferTx)
 	router.post('/:user/productHandoverTx', OrderController.productHandoverTx)
 	router.post('/:user/productDeliveredTx', OrderController.productDeliveredTx)
